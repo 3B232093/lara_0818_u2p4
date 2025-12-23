bcab019048daf00a0b613d12d22c07e06377175e
3B232093 <3b232093@gm.student.ncut.edu.tw>
2025-12-24 01:40:13 +0800

    子樣板index.blade.php繼承master主樣板

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
